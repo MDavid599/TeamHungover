5925bf534febc106b8cefeec8f5fa1b0197b3557
mwinata94 <mwinata@mail.sfsu.edu>
2019-03-03 16:47:21 +0000

    fix dresses not showing up

--- backend/weightapi/weightinput/management/commands/seed_macy_catalog.py
+++ backend/weightapi/weightinput/management/commands/seed_macy_catalog.py
@@ -77,11 +77,11 @@ def store_macy_catalog():
                     largest_size.filter(category_name__contains="dress")
             upper_waist = largest_size[0].upper_waist
             if (smallest_size[0].lower_waist > 0 and upper_waist < 1):
-                upper_waist = smallest_size[0].lower_waist
+                upper_waist = smallest_size[0].lower_waist + 8
             
             upper_hips = largest_size[0].upper_hips
             if smallest_size[0].lower_hips > 0 and upper_hips < 1:
-                upper_hips = smallest_size[0].lower_hips
+                upper_hips = smallest_size[0].lower_hips + 8
 
             body_size = BodySize.objects.filter(
                 lower_bust = smallest_size[0].lower_bust,
@@ -100,7 +100,7 @@ def store_macy_catalog():
             image_url = ""
             if "primaryPortraitSource" in datum["productDetails"]["summary"]:
                 image_url = datum["productDetails"]["summary"]["primaryPortraitSource"]
-            if not body_size.count:
+            if not len(body_size):
                 body_size = BodySize(
                     lower_bust = smallest_size[0].lower_bust,
                     upper_bust = largest_size[0].upper_bust,
